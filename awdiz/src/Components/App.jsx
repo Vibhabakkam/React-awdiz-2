1fe2651242cda0ec06cad16f53266053d17dbd8a
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-18 19:33:01 +0530

    updates

--- awdiz/src/Components/App.jsx
+++ awdiz/src/Components/App.jsx
@@ -1,11 +1,21 @@
 
 import Register from './Register';
+import Effect from './Effect';
+import Effect2 from './Effect2';
+import Home from './Home';
+import Login from './Login';
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div>
-        <Register/>  ;
+      <Routes>
+        <Route excat path='/Home' element={< Home />} />
+        <Route excat path='/Login' element={< Login />} />
+        <Route excat path='/Register' element={<Register />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
