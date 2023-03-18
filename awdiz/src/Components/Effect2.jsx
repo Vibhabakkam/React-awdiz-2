fe3d9071f98414be307b8eea7d3980acb1b0487e
Vibhabakkam <vibhabakkam22@gmail.com>
2023-03-18 19:33:13 +0530

    Create Effect2.jsx

---
+++ awdiz/src/Components/Effect2.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+const Effect2 = () => {
+    const [products, setProducts] = useState();
+    console.log (products, 'products here')
+
+    useEffect (() => {
+        fetch("https://fakestoreapi.com/products")
+        .then(res => res.json())
+        .then(json => setProducts (json))
+
+    }, []
+    )
+
+    return(
+        <>
+        <div id="products">
+  
+         { products && products.map((e,i) => (
+            <div key={i}>
+                <img src= {e.image} alt="" />
+                <h1> {e.title}</h1>
+                <p> {e.price}</p>
+                <p> 250 for each</p>
+            </div>
+         )  
+         )
+
+         }
+
+
+        </div>
+        
+        </>
+    )
+
+
+}
+export default Effect2;
